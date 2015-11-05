d47614a5ff6d36fb1983e1ce8d7714c9907d6ab3
johanncanon <johanncanon@gmail.com>
2015-11-05 16:16:59 -0500

    arreglos de beans de categorias, referencias y marcas.

--- src/java/Bean/MarcaBean.java
+++ src/java/Bean/MarcaBean.java
@@ -5,6 +5,12 @@
  */
 package Bean;
 
+import DAO.IMarcaDao;
+import DAO.ImpMarcaDao;
+import Modelo.SmsMarca;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -14,12 +20,72 @@ import javax.faces.bean.RequestScoped;
  */
 @ManagedBean
 @RequestScoped
-public class MarcaBean {
-
-    /**
-     * Creates a new instance of MarcaBean
-     */
+public class MarcaBean implements Serializable{
+    
+    private SmsMarca marca;
+    private List<SmsMarca> marcas;
+    private List<String> marcaSt;  
+    
+    
     public MarcaBean() {
+        marca = new SmsMarca();
+        marcaSt = new ArrayList<>();
     }
+    
+    
+    /* GETTERS Y SETTERS **********************************************************************/
+    public SmsMarca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(SmsMarca marca) {
+        this.marca = marca;
+    }
+
+    public List<SmsMarca> getMarcas() {
+        IMarcaDao marcaDao = new ImpMarcaDao();
+        marcas = marcaDao.mostrarMarcas();
+        return marcas;
+    }
+
+    public void setMarcas(List<SmsMarca> marcas) {
+        this.marcas = marcas;
+    }
+    
+    public List<String> getMarcaSt() {
+        IMarcaDao marcaDao = new ImpMarcaDao();
+        marcas = marcaDao.mostrarMarcas();
+        for (int i = 0; i < marcas.size(); i++) {
+            marcaSt.add(marcas.get(i).getMarcaNombre());
+        }
+        
+        return marcaSt;
+    }
+
+    public void setMarcaSt(List<String> marcaSt) {
+        this.marcaSt = marcaSt;
+    }
+    
+    /*+++++++++++++++++++++++++*****************************************************************
+    ****************    CREACION DE METODOS DEL BEAN      *********************************+++++*/
+    
+    public void modMarca(){
+        IMarcaDao marcaDao = new ImpMarcaDao();
+        marcaDao.modificarMarca(marca);
+        marca = new SmsMarca();
+    }
+    
+    public void eliMarca(){
+        IMarcaDao marcaDao = new ImpMarcaDao();
+        marcaDao.eliminarMarca(marca);
+        marca = new SmsMarca();
+    }
+    
+    public void regMarca(){
+        IMarcaDao marcaDao = new ImpMarcaDao();
+        marcaDao.registrarMarca(marca);
+        marca = new SmsMarca();
+    }
+    
     
 }
