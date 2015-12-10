c9484e1321afbd1dd3a0258fe607b3d4c166c83b
johanncanon <johanncanon@gmail.com>
2015-12-10 08:32:41 -0500

    arreglo de vistas de admin principal y secundario

---
+++ src/java/Bean/cargaArchivosBean.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+/**
+ *
+ * @author SISTEMAS
+ */
+@Named(value = "cargaArchivosBean")
+@Dependent
+public class cargaArchivosBean {
+
+    private UploadedFile archivo;
+
+    public UploadedFile getArchivo() {
+        return archivo;
+    }
+
+    public void setFile(UploadedFile archivo) {
+        this.archivo = archivo;
+    }
+
+    public void upload() {
+        if (archivo != null) {
+            FacesMessage message = new FacesMessage("exito, el archivo ", archivo.getFileName() + " ha sido cargado.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+}
